e80db031251ecbd7e135d3dab2b63f5c240573fe
Cheonghee Kim <hello8965@gmail.com>
2017-12-07 18:32:28 +0900

    5page project second upload (~introP) 12/07/thr

--- step_05_project/5p_project/js/src/index.js
+++ step_05_project/5p_project/js/src/index.js
@@ -1,2 +1,40 @@
 //index.js
 
+(function($){ 
+
+var banner = $('.banner');
+var bannerUl = banner.children('ul');
+var bannerLast = bannerUl.children('li');
+
+bannerLast.eq(-1).prependTo(bannerUl);
+
+ bannerUl.css({marginLeft:'-100%'});
+ banner.css({overflow:'hidden'});
+
+var leftBtn = $('.btn').children('button').first();
+var rightBtn = $('.btn').children('button').last();
+
+leftBtn.on('click',function(event){
+event.preventDefault(); 
+
+bannerUl.stop(false,true).animate({marginLeft:0}, function(){
+  bannerLast = bannerUl.children('li').last();
+  bannerLast.prependTo(bannerUl);
+  bannerUl.css({marginLeft:0});
+    });
+  }); //LeftBtn 클릭
+
+rightBtn.on('click',function(event){
+event.preventDefault(); 
+
+bannerUl.stop(false,true).animate({marginLeft:'-200%'}, function(){
+bannerLast = bannerUl.children('li').first();
+bannerLast.appendTo(bannerUl);
+bannerUl.css({marginLeft:'-100%'});
+  });//rightBtn 클릭
+
+  });
+
+
+
+})(this.jQuery);
